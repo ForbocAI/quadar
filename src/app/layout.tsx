c729a58f857a6c835d06216fa4e7b57cc6343f0f
Sean Dinwiddie <seandinwiddie@gmail.com>
2026-03-23 05:26:11 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
         {`window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-D999WBQEXY');`}
+gtag('config', '');`}
       </Script>
       <body className="font-sans antialiased bg-palette-bg-dark">
         <StoreProvider>
